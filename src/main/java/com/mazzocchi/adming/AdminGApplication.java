5f7fb95004dd4507aa45285879b0cd57dd9f2109
Bruno Mazzocchi <bemazzocchi@gmail.com>
2023-01-04 17:43:53 -0600

    Initial commit:
    :

---
+++ src/main/java/com/mazzocchi/adming/AdminGApplication.java
@@ -0,0 +1,13 @@
+package com.mazzocchi.adming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminGApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminGApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mazzocchi/adming/AdminGApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazzocchi.adming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminGApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
